9c077ab3cea3f919379c1921b68750a31014d3b1
DAZUN XING <dazun.xing@gmail.com>
2020-05-17 14:30:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  @@items=[]
+  def initialize(discount=0)
+    @total =0
+    @discount=discount
+  end
+  
+ def discount
+   @discount
+ end
+  
+  # def total
+  #   @total 
+  # end
+  
+   def add_item(title,price,quantity=1)
+     item =[]
+    @total +=  price*quantity
+    # quantity.times {@@items << title}
+     items << title
+  end
+  
+  def apply_discount
+    @total =@total*(100-@discount)/100
+    if !(@discount==0)
+      return "After the discount, the total comes to $#{@total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+  
+  def items
+    items
+  end
+end
